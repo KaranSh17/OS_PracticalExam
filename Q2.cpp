0feb3d858a8c12cdf779bb81784aa2e3bea1f87f
KaranSh17 <95768235+KaranSh17@users.noreply.github.com>
2021-12-09 11:08:16 +0530

    Add files via upload

---
+++ Q2.cpp
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <iostream>
+//#include <sys/wait.h>
+#include <unistd.h>
+
+using namespace std;
+
+int main()
+{
+    pid_t pid;
+	/* fork a child process */
+	pid = fork();
+    if (pid < 0)
+    {
+		/* error occurred */
+		cout<<stderr<< "Fork Failed"<<endl;
+        return 1;
+    }
+    else if (pid == 0)
+    {
+		/* child process */
+		execlp("/bin/ls","ls",NULL);
+    }
+    else
+    {
+		/* parent process */
+		/* parent will wait for the child to complete */
+		wait(NULL);
+        cout<<"Parent Complete";
+    }
+    return 0;
+}
